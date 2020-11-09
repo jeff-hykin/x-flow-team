c47c5513ad2f0756cd1cc6a293ae3995b1baa390
jeff-hykin <jeff.hykin@gmail.com>
2020-11-09 19:31:00 +0000

    init environment

---
+++ README.md
@@ -0,0 +1,37 @@
+## How to setup (The not-recommended-way)
+* install python3
+* install the pip modules in `requirements.txt`
+* add the project directory to your `PYTHONPATH` env variable
+* look at files in `./settings/commands/` to see actions for the project
+
+# How to setup (Prefered way)
+
+### For Windows
+
+* Get [WSL](https://youtu.be/av0UQy6g2FA?t=91) (Windows Subsystem for Linux) or [WSL2](https://www.omgubuntu.co.uk/how-to-install-wsl2-on-windows-10)<br>
+    * If you're not familiar with WSL, I'd recommend [watching a quick thing on it like this one](https://youtu.be/av0UQy6g2FA?t=91)
+    * Ubuntu 18.04 for WSL is preferred (same as in that linked video), but Ubuntu 20.04 or similar should work.
+    * [WSL2](https://www.omgubuntu.co.uk/how-to-install-wsl2-on-windows-10) (just released August 2020) is needed if you want to use your GPU.<br>
+* Once WSL is installed (and you have a terminal logged into WSL) follow the Mac/Linux instructions below.
+
+### For Mac/Linux
+
+* Install [nix](https://nixos.org/guides/install-nix.html), more detailed guide [here](https://nixos.org/manual/nix/stable/#chap-installation)
+    * Just run the following in your console/terminal app
+        * `sudo apt-get update 2>/dev/null`
+        * If you're on MacOS Catalina, run:
+            * `sh <(curl -L https://nixos.org/nix/install) --darwin-use-unencrypted-nix-store-volume `
+        * If you're not, run:
+            * `curl -L https://nixos.org/nix/install | bash`
+        * `source $HOME/.nix-profile/etc/profile.d/nix.sh`
+        * (may need to restart console/terminal)
+* Install `git`
+    * (if you don't have git just run `nix-env -i git`)
+* Clone/Open the project
+    * `cd wherever-you-want-to-save-this-project`<br>
+    * `git clone https://github.com/jeff-hykin/x-flow-team`
+    * `cd x-flow-team`
+* Actually run some code
+    * run `nix-shell` to get into the project enviornment
+        * Note: this will almost certainly take a while the first time because it will auto-install exact versions of everything: `python`, `pip`, the python virtual enviornment, all of the pip modules, and auto-setup the env variables like the PYTHONPATH.
+    * run `commands` to see all of the project commands
---
+++ shell.nix
@@ -0,0 +1,89 @@
+# 
+# how to add packages?
+# 
+    # you can search for them here: https://search.nixos.org/packages
+    # to find them in the commandline use:
+    #     nix-env -qP --available PACKAGE_NAME_HERE | cat
+    # ex:
+    #     nix-env -qP --available opencv
+    #
+    # NOTE: some things (like setuptools) just don't show up in the 
+    # search results for some reason, and you just have to guess and check 🙃 
+
+# Lets setup some definitions
+let        
+    definitions = rec {
+        # 
+        # load the package.json cause were going to extract basically everything from there
+        # 
+        packageJson = builtins.fromJSON (builtins.readFile ./package.json);
+        # 
+        # load the store with all the packages, and load it with the config
+        # 
+        mainRepo = builtins.fetchTarball {url="https://github.com/NixOS/nixpkgs/archive/${packageJson.nix.mainRepo}.tar.gz";};
+        mainPackages = builtins.import mainRepo {
+            config = packageJson.nix.config;
+        };
+        # 
+        # reorganize the list of packages into a list like:
+        #    [ { name: "blah", commitHash:"blah", source: (*an object*) }, ... ]
+        # 
+        packagesWithSources = builtins.map (
+            each: ({
+                name = each.load;
+                commitHash = each.from;
+                source = builtins.getAttr each.load (
+                    builtins.import (
+                        builtins.fetchTarball {url="https://github.com/NixOS/nixpkgs/archive/${each.from}.tar.gz";}
+                    ) {
+                        config = packageJson.nix.config;
+                    }
+                );
+            })
+        ) packageJson.nix.packages;
+    };
+    
+    packagesForPython = [] ++ definitions.mainPackages.lib.optionals (definitions.mainPackages.stdenv.isDarwin) [
+        # python and venv
+        definitions.mainPackages.python37
+        definitions.mainPackages.python37Packages.setuptools
+        definitions.mainPackages.python37Packages.pip
+        definitions.mainPackages.python37Packages.virtualenv
+    ];
+# using those definitions
+in
+    # create a shell
+    definitions.mainPackages.mkShell {
+        # inside that shell, make sure to use these packages
+        buildInputs = packagesForPython ++ builtins.map (each: each.source) definitions.packagesWithSources;
+        
+        # run some bash code before starting up the shell
+        shellHook = ''
+        # we don't want to give nix or other apps our home folder
+        if [[ "$HOME" != "$(pwd)" ]] 
+        then
+            #
+            # find and run all the startup scripts in alphabetical order
+            #
+            for file in ./settings/shell_startup/#pre_changing_home/*
+            do
+                # make sure its a file
+                if [[ -f $file ]]; then
+                    source $file
+                fi
+            done
+            
+            mkdir -p .cache/
+            ln -s "$HOME/.cache/nix" "./.cache/" &>/dev/null
+            
+            # so make the home folder the same as the project folder
+            export HOME="$(pwd)"
+            # make it explicit which nixpkgs we're using
+            export NIX_PATH="nixpkgs=${definitions.mainRepo}:."
+            
+            # start zsh
+            nix-shell --pure --command zsh
+            exit
+        fi
+        '';
+    }
