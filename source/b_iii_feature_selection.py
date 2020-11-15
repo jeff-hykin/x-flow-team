5f95c7e36e1eb3b5993e10c75a772adc9fb17019
feras.khemakhem <foufoukhemakhem@gmail.com>
2020-11-15 15:59:39 -0600

    fischer criterion completed

--- source/b_iii_feature_selection.py
+++ source/b_iii_feature_selection.py
@@ -9,6 +9,7 @@ from skimage.util import img_as_float
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
 
+from sklearn.linear_model import LogisticRegression
 from mlxtend.feature_selection import SequentialFeatureSelector as SFS
 from sklearn.neighbors import KNeighborsClassifier as KNN
 
@@ -16,9 +17,10 @@ from misc_tools import split_data
 from b_i_visual_feature_extraction import hog_feature, create_feature_df
 
 
+# https://www.codespeedy.com/sequential-forward-selection-with-python-and-scikit-learn/
 def sequential_forward_selection(data, num_tests, feature_counts):
 	'''
-	inputs: DataFrame including features and labels ('classes'), 
+	inputs: DataFrame including features and labels ('covid(label)'), 
 					(hyperparam) number of iterations of SFS to be ran,
 					(hyperparam) number of features,
 	output: list of features chosen,
@@ -59,41 +61,93 @@ def sequential_forward_selection(data, num_tests, feature_counts):
 # calculate fischer criterion value for feature d
 def fischer_criterion(data, d):
 	'''
-	inputs: DataFrame including features and labels ('classes'),
+	inputs: DataFrame including features and labels ('covid(label)'),
 					feature 'd',
 	output: fischer criterion score for feature 'd',
 	'''
+	# numer:
+	# for each class
+	# for each feature value in class
+	# val - avg(all values of given feature in class)
+	# denom:
+	# for each class
+	# (average of all feature values ever) - (average of all feature values in class)
 	classes = train_labels.ix[:,0].unique() # number of classes
-	for k, _class in enumerate(classes): # for each class
-		average = 1
-	return 1
+	numerator = 0
+	denomenator = 0
+	ud = data[d].mean() # average of all values in feature / mu d
+	for k in classes: # for each class / sum k
+		vals_in_class = data.loc[data['covid(label)'] == k] # create subdf with all values in given class
+		ukd = vals_in_class[d].mean() # average of feature values in given class / mu kd
+		for xnd in vals_in_class: # for each feature value in class / sum xn
+			numerator += (xnd - ukd) ** 2
+		denomenator += (ud - ukd) ** 2
+
+	# calculate final fischer's criterion
+	fc = numerator / denomenator
+
+	return fc
 
 	
 
-def fischer_criterion_search(data, feature_counts):
+def fischer_criterion_selection(data, feature_counts):
 	'''
 	inputs: DataFrame including features and labels ('classes'),
 					list with different numbers of features to test with,
 	output: list of features chosen,
 	'''
 	# split training data into validation and training subsets
-	train_ratio = 0.8 # ratio of training size to validation size
+	train_ratio = 0.75 # ratio of training size to validation size
 	data_split = split_data(data, 2, training_ratio)
 
 	features_and_scores = [] # tuple of feature and score
 	# calculate criterion for each feature
-	for feature in range(features):
-		features_and_scores.append((feat, fischer_criterion(label_list[0], feat)))
+	features = data.keys()
+
+	for feature in features:
+		if feature != 'filename' and feature != 'covid(label)': # ignore filename and label
+			features_and_scores.append(( feature , fischer_criterion(data_split[0],feature) ))
 
 	# reorder array by increasing score (inplace)
 	features_and_scores.reorder(key=lambda tup: tup[1])
 
 	# run validation subset for different feature counts
-	best_nfeat = 0
-	# for nfeat in range(feature_counts):
-		# test with validation set with x features
-		
-	return 1
+	val_labels = data[1]['covid(label)'].copy()
+	train_labels = data[0]['covid(label)'].copy()
+	best_features = {'accuracy': 0, 'count': 0}
+	prev_nfeat = 0
+	val_features = pd.DataFrame()
+	train_features = pd.DataFrame()
+
+	# test for different numbers of features
+	for nfeat in feature_counts:
+
+		# create features list for testing
+		if ifeat in range(prev_nfeat, nfeat): # do not bother re-add already existing features
+			f = features_and_score[ifeat][0] # extract feature as f
+			val_features = val_features.join(data_split[1][f]) # add to dataframe
+			train_features = train_features.join(data_split[0][f]) # add to dataframe
+
+		# test with validation set with nfeat features
+		clf = LogisticRegression(random_state=0).fit(train_features, train_labels)
+		score = clf.score(val_features, val_labels)
+		print('CV score for', nfeat, 'features:\n', score)
+
+		# if score is better, update
+		if score > best_features['accuracy']:
+			best_features = {'accuracy': score, 'count': nfeat}
+
+		# update prev number of features for next iteration
+		prev_nfeat = nfeat
+
+	# create array of optimal features
+	optimal_features = []
+	optimal_nfeats = best_features['count'] # best number of people
+	for optimal_feature, optimal_score in features_and_scores[:optimal_nfeats]:
+		optimal_features.append(optimal_feature)
+
+	print('Best Fischer Criterion features accuracy is', best_features['accuracy'], 'with', best_features['count'], 'features at indicies', optimal_features)
+	return optimal_features
 
 # main function
 if __name__ == "__main__":
@@ -109,4 +163,4 @@ if __name__ == "__main__":
 	sequential_forward_selection(df_hog, num_tests, feature_counts)
 
 	# filter feature selection - fischer testing with different feature counts
-	fischer_criterion_search(df_hog, feature_counts)
+	fischer_criterion_selection(df_hog, feature_counts)
